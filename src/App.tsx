818100a439d4f3e5c5b5793abe5124bf96b9305f
Cristina <cristinaforescampos1992@gmail.com>
2023-04-20 19:20:07 +0200

    Add custom hook usePost( getPosts)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import usePosts from "./hooks/usePost/usePosts";
+
 function App() {
+  const { getPosts } = usePosts();
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
   return <div></div>;
 }
 
---
+++ src/hooks/usePost/usePosts.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useAppDispatch } from "../../redux/hooks";
+import {
+  hiddenLoadingActionCreator,
+  showLoadingActionCreator,
+  showModalActionCreator,
+} from "../../redux/features/uiSlice/uiSlice";
+import { loadPostsActionCreator } from "../../redux/features/postSlice/postsSlice";
+import { useCallback } from "react";
+import { Post } from "../../redux/features/postSlice/types";
+
+const usePosts = () => {
+  const dispatch = useAppDispatch();
+  const urlApi = "https://jsonplaceholder.typicode.com/";
+
+  const getPosts = useCallback(async () => {
+    dispatch(showLoadingActionCreator());
+    try {
+      const { data } = await axios.get<Post[]>(`${urlApi}posts`);
+
+      dispatch(loadPostsActionCreator(data));
+      dispatch(hiddenLoadingActionCreator());
+    } catch (error: unknown) {
+      dispatch(
+        showModalActionCreator({
+          isError: true,
+          text: "No hay ningun post disponible",
+        })
+      );
+    }
+  }, [dispatch]);
+
+  return { getPosts };
+};
+
+export default usePosts;
