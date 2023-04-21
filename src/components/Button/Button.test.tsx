30afa33933ba847ca5710cb41f5b33b688b1d05a
Cristina <cristinaforescampos1992@gmail.com>
2023-04-21 23:57:44 +0200

    Test Button component

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,25 @@
+import { renderWithProviders } from "../../mocks/renderWithProviders";
+import Button from "./Button";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("Given component Button", () => {
+  describe("When it render", () => {
+    test("Then its should show button with text:'Delete' and the action on click", async () => {
+      const buttonAction = jest.fn();
+
+      renderWithProviders(
+        <Button text="Delete" ariaLabel={"Delete"} action={buttonAction} />
+      );
+
+      const expectedButton = screen.queryByRole("button", {
+        name: /Delete/i,
+      });
+
+      await userEvent.click(expectedButton!);
+
+      expect(expectedButton).toBeInTheDocument();
+      expect(buttonAction).toHaveBeenCalled();
+    });
+  });
+});
