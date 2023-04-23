f1be405a68fc2c40e7c2267a1e2c25dba91b9f30
Cristina <cristinaforescampos1992@gmail.com>
2023-04-23 18:55:45 +0200

    Create component Layout and customize stlyles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,10 @@
-import { useEffect } from "react";
-import usePosts from "../../hooks/usePost/usePosts";
-import ListPosts from "../ListPosts/ListPosts";
+import Layout from "../Layout/Layout";
 import AppStyled from "./AppStyled";
-import { useAppSelector } from "../../redux/hooks";
-import Modal from "../Modal/Modal";
 
 function App() {
-  const { getPosts } = usePosts();
-  const {
-    modal: { showModal },
-  } = useAppSelector((state) => state.ui);
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
   return (
     <AppStyled>
-      {showModal && <Modal />}
-      <ListPosts />
+      <Layout />
     </AppStyled>
   );
 }
--- src/components/App/AppStyled.tsx
+++ src/components/App/AppStyled.tsx
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
-const AppStyled = styled.div`
-  padding: 2rem;
-`;
+const AppStyled = styled.div``;
 
 export default AppStyled;
